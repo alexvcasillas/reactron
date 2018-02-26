888a07d7234bc8663b72bdc1261bb9b8032ec86b
Alex Casillas <alexvcasillas@gmail.com>
2018-02-27 00:38:41 +0100

    Parcel + Electron combined

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const { setMainMenu } = require('./app-menu');
+const execa = require('execa');
+
+require('electron-debug')({ showDevTools: true });
 
 let mainWindow;
 
-app.on('ready', () => {
+function runParcel() {
+  return new Promise(resolve => {
+    let output = '';
+    const parcelProcess = execa('parcel', ['index.html']);
+    const concat = chunk => {
+      output += chunk;
+      console.log(output);
+      if (output.includes('Built in ')) {
+        parcelProcess.stdout.removeListener('data', concat);
+        console.log(output);
+        resolve();
+      }
+    };
+    parcelProcess.stdout.on('data', concat);
+  });
+}
+
+app.on('ready', async () => {
   // Create the browser main window
   mainWindow = new BrowserWindow({
-    show: false
+    show: false,
   });
+  await runParcel();
   // Set the load URL
-  mainWindow.loadURL(path.join('file://', __dirname, 'index.html'));
+  mainWindow.loadURL('http://localhost:1234');
   // Set the menu of the main window's application
   setMainMenu();
   // When the main window is ready, show it
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HelloMessage extends React.Component {
+  render() {
+    return <div>Hello {this.props.name}</div>;
+  }
+}
+
+var mountNode = document.getElementById('root');
+ReactDOM.render(<HelloMessage name="Alex" />, mountNode);
